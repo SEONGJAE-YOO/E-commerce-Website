088638315eeeeab66789f6b43145532a214458e7
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-19 15:57:14 +0900

    Added profile function

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from userauths.models import User, Profile
 
-# class UserAdmin(admin.ModelAdmin):
-#     search_fields  = ['full_name', 'username', 'email',  'phone']
-#     list_display  = ['username', 'email', 'phone']
+class UserAdmin(admin.ModelAdmin):
+    search_fields  = ['full_name', 'username', 'email',  'phone']
+    list_display  = ['username', 'email', 'phone']
 
-# class ProfileAdmin(admin.ModelAdmin):
-#     search_fields  = ['user']
-#     list_display = ['thumbnail', 'user', 'full_name']
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields  = ['full_name']
+    list_filter = ['date']
+    list_display = ['full_name', 'user', 'full_name']
 
 
-admin.site.register(User)
-admin.site.register(Profile)
+admin.site.register(User,UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from shortuuid.django_fields import ShortUUIDField
-
+from django.db.models.signals import post_save
 
 
 GENDER = (
@@ -18,7 +18,7 @@ class User(AbstractUser):
     otp = models.CharField(max_length=1000, null=True, blank=True)
     reset_token  = models.CharField(max_length=1000, null=True, blank=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email' # # 더 이상 username이 아닌 email을 사용하여 로그인 및 계정 생성을 진행할 수 있게 됩니다.
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
@@ -70,3 +70,15 @@ class Profile(models.Model):
              self.full_name = self.user.full_name
         
         super(Profile, self).save(*args, **kwargs)
+
+
+   
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
