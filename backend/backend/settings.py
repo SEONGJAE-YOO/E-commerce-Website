680f2efc5cbb6994eeec085f289470133dfe757a
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-19 17:10:03 +0900

    Implemented POST endpoint (/api/v1/user/token/) for generating tokens using email and password with Django Rest Framework.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +46,10 @@ INSTALLED_APPS = [
     'vendor',
     'customer',
     'api',
+    
+    # Third party apps
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     
 ]
 
@@ -131,13 +136,48 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-AUTH_USER_MODEL = 'userauths.User'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+
+AUTH_USER_MODEL = 'userauths.User'
 
 JAZZMIN_SETTINGS = {
     "site_title": "E-commerce Admin Panel",
--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# Restframework
+from rest_framework_simplejwt.views import TokenObtainPairView
+# Serializers
+from userauths.serializer import MyTokenObtainPairSerializer
 
-# Create your views here.
+
+# This code defines a DRF View class called MyTokenObtainPairView, which inherits from TokenObtainPairView.
+class MyTokenObtainPairView(TokenObtainPairView):
+    # Here, it specifies the serializer class to be used with this view.
+    serializer_class = MyTokenObtainPairSerializer
