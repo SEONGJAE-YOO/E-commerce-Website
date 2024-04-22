92d8281defd8eb1a874f6c248d86bb90baaf1f8f
YooSeongJae <yuseong12345@naver.com>
2024-04-22 17:27:59 +0900

    Implemented function to set authenticated user upon page load

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from userauths import views as userauths_views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/register/', userauths_views.RegisterView.as_view(), name='auth_register'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,8 +23,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
+schema_view = get_schema_view(   
+   openapi.Info( 
       title="E-commerce Backend APIs",
       default_version='v1',
       description="This is the API documentation for e-commerce project APIs",
