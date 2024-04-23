e49eee8719a21a7554b5edbf5bbbdc5f2cc885de
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-24 02:29:41 +0900

    Implemented logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'; // Import the 'Routes', 'Route', and 'BrowserRouter' components.
 import Login from './views/auth/Login'; // Import the 'Login' component.
 import Register from './views/auth/Register'; // Import the 'Register' component.
+import Logout from './views/auth/Logout'; // Import the 'Logout' component.
 
 function App() { // Define the main 'App' component.
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() { // Define the main 'App' component.
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path='/register' element={<Register />} />      
+        <Route path='/logout' element={<Logout />} />
       </Routes>
     </BrowserRouter> 
   )
