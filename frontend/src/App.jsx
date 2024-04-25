9b1345cf9734b78bbe0ea7e0087c6d327d62fde7
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-25 18:26:25 +0900

    Applied Bootstrap to the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
 import {useState} from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'; // Import the 'Routes', 'Route', and 'BrowserRouter' components.
+// import Home from './views/shop/home'; // Importing the 'Home' component.
 import Login from './views/auth/Login'; // Import the 'Login' component.
 import Register from './views/auth/Register'; // Import the 'Register' component.
 import Logout from './views/auth/Logout'; // Import the 'Logout' component.
 import ForgotPassword from './views/auth/ForgotPassword'; // Import the 'ForgotPassword' component.
+import CreatePassword from './views/auth/createPassword';
+import StoreHeader from './views/base/StoreHeader';
+import StoreFooter from './views/base/StoreFooter';
+// import MainWrapper from './layouts/MainWrapper';
+
 
 function App() { // Define the main 'App' component.
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path='/register' element={<Register />} />      
-        <Route path='/logout' element={<Logout />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
+      <StoreHeader />
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path='/register' element={<Register />} />      
+            <Route path='/logout' element={<Logout />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/create-new-password" element={<CreatePassword />} />
+          </Routes>
+      <StoreFooter />
     </BrowserRouter> 
   )
 }
