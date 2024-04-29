c09c7d5a4b4bdfa30065a2c6d19bc4154022dd70
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-29 16:38:15 +0900

    Created ORM for Category and Product models

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from userauths.models import User, Profile
+from vendor.models import Vendor
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.text import slugify
 
-# Create your models here.
+# Model for Product Categories
+class Category(models.Model):
+    id = models.BigAutoField(help_text="Category ID", primary_key=True)
+    # Category title
+    title = models.CharField(max_length=100)
+    # Image for the category
+    image = models.FileField(upload_to="category", default="category.jpg", null=True, blank=True)
+    # Is the category active?
+    active = models.BooleanField(default=True)
+    # Slug for SEO-friendly URLs
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ["title"]
+    
+# Model for Products
+class Product(models.Model):
+    
+    STATUS = (
+        ("draft", "Draft"),
+        ("disabled", "Disabled"),
+        ("rejected", "Rejected"),
+        ("in_review", "In Review"),
+        ("published", "Published"),
+    )  
+    id = models.BigAutoField(help_text="Product ID", primary_key=True)
+    # Product title
+    title = models.CharField(max_length=100)
+    # Image for the product
+    image = models.FileField(upload_to="products", blank=True, null=True, default="product.jpg")
+    # Description for the product using HTML
+    description = models.TextField(null=True, blank=True)
+    
+    # Categories that the product belongs to
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    # Price of the product
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
+    shipping_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
+    stock_qty = models.PositiveIntegerField(default=0)
+    in_stock = models.BooleanField(default=True)
+    status = models.CharField(max_length=100, default="published", choices=STATUS)
+    featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+    rating = models.PositiveIntegerField(default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, blank=True, related_name="Vendor")
+    
+    pid = ShortUUIDField(unique=True, length=10, alphabet="abcdefghijklmnopqrstuvxyz")
+    slug = models.SlugField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug is None:
+            self.slug = slugify(self.name)    
+        super(Product, self).save(*args, **kwargs) 
+        
+    def __str__(self):
+        return self.title
