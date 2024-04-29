e05a7e95691975b200a99333ca71e39157d8125c
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2024-04-29 21:23:55 +0900

    ✨ feat: Implemented Category and Product APIs using Django Rest Framework serializers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from userauths import views as userauths_views
 from rest_framework_simplejwt.views import TokenRefreshView
+from store import views as store_views
+
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -9,4 +11,8 @@ urlpatterns = [
     path('user/password-reset/<email>/', userauths_views.PasswordEmailVerify.as_view(), name='password_reset'),
     path('user/password-change/', userauths_views.PasswordChangeView.as_view(), name='password_change'),
 
+
+    #Store Endpoints
+    path('category/',  store_views.CategoryListAPIView.as_view(), name='category_list'),
+    path('product/',  store_views.ProductListAPIView.as_view(), name='product_list'),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from store.models import Category, Product
+from store.serializer import CategorySerializer, ProductSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+    
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
